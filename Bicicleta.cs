59af082b8322aaa0e7ed97b95a3eba50a5c7dbd7
Ian Serrão <ianserraoribeiro2006@gmail.com>
2025-11-12 15:54:23 -0300

    Add files via upload

---
+++ Bicicleta.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bicicleta : MonoBehaviour
+{
+    public float forceAmount = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        bool aPressed = Input.GetKey(KeyCode.A);
+        bool dPressed = Input.GetKey(KeyCode.D);
+
+        // Verifica se ambas estão pressionadas AO MESMO TEMPO
+        if (aPressed && dPressed)
+        {
+            // Aplica força para frente
+            rb.AddForce(transform.forward * forceAmount, ForceMode.Acceleration);
+        }
+    }
+}
